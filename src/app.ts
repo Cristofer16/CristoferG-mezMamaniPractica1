c07d089dbc00bcb599185ead3d17883232cdd517
Cristofer Gómez <cristofergm123@gmail.com>
2021-04-15 01:51:22 -0400

    problema6

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
-function agregar(arre: number[]) {
-    let x: number[] = arre.sort();
-    let max: number = arre.length-1;
-    let m: number = x[0];
-    let cont: number = 0;
-    for(let i: number = 0; i <= max; i++) {
-        while(m != x[i]) {
-            cont++;
-            m++;
-        }
-        m++;
+function costo(arre: number[][]) {
+    let x: number[] = [arre[0].length];
+    for(let i: number = 0; i < arre[0].length; i++) {
+        x[i] = 1;
     }
-    return cont;
+    let cont: number = 0;
+    let suma: number = 0;
+    for(let i: number = 0; i < arre.length; i++) {
+        for(let j: number = 0; j < arre[0].length; j++) {
+            if(cont == arre[i].length) {
+                return suma;
+            }
+            if(x[j] == 1) {
+                if(arre[i][j] == 0) {
+                    x[j] = 0;
+                    cont++;
+                }
+                suma += arre[i][j];
+            }
+        }
+    }
+    return suma;
 }
-let a: number[] =[0, 3];
-console.log(agregar(a));
+let a: number[][] =[[1,1,1],[2,2,2],[3,3,3]];
+console.log(costo(a));
