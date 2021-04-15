f8038b3a0f2ed1b78bb3c8c2ac375707baeff6fb
Cristofer Gómez <cristofergm123@gmail.com>
2021-04-15 02:09:34 -0400

    problema7

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,22 @@
-function costo(arre: number[][]) {
-    let x: number[] = [arre[0].length];
-    for(let i: number = 0; i < arre[0].length; i++) {
-        x[i] = 1;
+function costo(x: string, y: string) {
+    let n: number[] = [y.length];
+    for (let i: number = 0; i < y.length; i++) {
+        n[i] = 1;
     }
     let cont: number = 0;
-    let suma: number = 0;
-    for(let i: number = 0; i < arre.length; i++) {
-        for(let j: number = 0; j < arre[0].length; j++) {
-            if(cont == arre[i].length) {
-                return suma;
-            }
-            if(x[j] == 1) {
-                if(arre[i][j] == 0) {
-                    x[j] = 0;
+    for(let i: number = 0; i < x.length; i++) {
+        for(let j: number = 0; j < y.length; j++) {
+            if(n[j] == 1) {
+                if(x.charAt(i) == y.charAt(j)) {
+                    n[j] = 0;
                     cont++;
+                    break;
                 }
-                suma += arre[i][j];
             }
         }
     }
-    return suma;
+    return cont;
 }
-let a: number[][] =[[1,1,1],[2,2,2],[3,3,3]];
-console.log(costo(a));
+let a:string = "abca";
+let b:string = "xyzbac";
+console.log(costo(a, b));
