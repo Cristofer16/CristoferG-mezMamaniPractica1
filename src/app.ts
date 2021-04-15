e5dabb5e28455e0b8d05ed30156a912a3a633d71
Cristofer Gómez <cristofergm123@gmail.com>
2021-04-14 23:54:22 -0400

    problema1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("your solutions of the problems goes here")
+let a: number = 83;
+function primo(a: number) {
+    for (let i: number = 2; i < a/2; i++) {
+        if (a%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+if (primo(a)) {
+    console.log("Es primo");
+} else {
+    console.log("No es primo");
+}
